7b05396604172efd36559e516dca9a26b3224e0c
emax-77 <peter.wirth@gmail.com>
2025-12-17 17:56:52 +0100

    Screenshots created

--- SimpleNavigator/Platforms/iOS/AppDelegate.cs
+++ SimpleNavigator/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace SimpleNavigator;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
